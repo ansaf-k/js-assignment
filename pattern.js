5bc3066929d6a20d5e7a36d4db23f176a9a11317
ansaf-k <ansafk369@gmail.com>
2025-01-01 10:51:14 +0530

    pattern

---
+++ pattern.js
@@ -0,0 +1,12 @@
+const words = 'MALAYALAM';
+let newWord = ''
+
+// for (var i = 0; i < (words.length + 1); i++) {
+//         console.log(words.substring(0,i));
+// }
+
+for (let i = 0; i < 9; i++) {
+    newWord += words[i] 
+    console.log(newWord)
+}
+    
